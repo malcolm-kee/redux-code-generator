ad7986223f049927ebe055960e1747c80a64efd1
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-01-28 09:46:05 +0800

    make generate-type to use single fn

--- src/code-generators/generate-type.ts
+++ src/code-generators/generate-type.ts
@@ -6,11 +6,7 @@ import getWriter from './get-writer';
 export const getStoreTypeName = (prefix: string) =>
   prefix ? `${capitalize(prefix)}Store` : 'RootStore';
 
-function writeType(writer: CodeBlockWriter, name: string, type: string) {
-  writer.writeLine(`readonly ${name}: ${type};`);
-}
-
-function writeObjectType(writer: CodeBlockWriter, object: any) {
+function writeType(writer: CodeBlockWriter, object: any) {
   if (object) {
     writer.inlineBlock(() => {
       Object.keys(object).forEach(key => {
@@ -19,23 +15,22 @@ function writeObjectType(writer: CodeBlockWriter, object: any) {
         const value = isArray ? object[key][0] : object[key];
         const valType = typeof value;
 
-        if (!isNil(value)) {
-          if (isBoolStrNum(valType)) {
-            writeType(writer, key, isArray ? `${valType}[]` : valType);
-          } else {
-            if (isArray) {
-              writer.write(`readonly ${key}: Array<`);
-              writeObjectType(writer, value);
-              writer.write('>;');
-              // writeTypeForObjectArray(writer, value, key);
-            } else {
-              writer.write(`readonly ${key}: `).inlineBlock(() => {
-                writeObjectType(writer, value);
-              });
-            }
-          }
+        if (isNil(value)) {
+          writer.writeLine(`readonly ${key}: ${isArray ? 'any[]' : 'any'};`);
+        } else if (isBoolStrNum(valType)) {
+          writer.writeLine(
+            `readonly ${key}: ${isArray ? `${valType}[]` : valType};`
+          );
         } else {
-          writeType(writer, key, isArray ? 'any[]' : 'any');
+          if (isArray) {
+            writer.write(`readonly ${key}: Array<`);
+            writeType(writer, value);
+            writer.write('>;');
+          } else {
+            writer.write(`readonly ${key}: `).inlineBlock(() => {
+              writeType(writer, value);
+            });
+          }
         }
       });
     });
@@ -49,7 +44,7 @@ export const generateType = (storeInitialState: any, prefix: string = '') => {
     .writeLine(prefix ? `// ${prefix}.type.ts` : `// type.ts`)
     .write(`export interface ${getStoreTypeName(prefix)} `);
 
-  writeObjectType(writer, storeInitialState);
+  writeType(writer, storeInitialState);
 
   return writer.toString();
 };
